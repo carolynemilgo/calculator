015282bca6daeb61c761b8b02dab5de04ac8dc5b
carolynemilgo <carolynemilgo@gmail.com>
2017-05-30 14:22:31 +0300

    added new functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-var add = function(number1, number2) {
+      var add = function(number1, number2)
+      {
         return number1 + number2;
       };
 
@@ -6,4 +7,28 @@ var add = function(number1, number2) {
       var number2 = parseInt(prompt("Enter another number:"));
 
       alert(add(number1, number2));
-      alert(result);
+
+      var divide = function(number1, number2) {
+              return number1 / number2;
+            };
+
+      var number1 = parseInt(prompt("Enter a number:"));
+      var number2 = parseInt(prompt("Enter another number:"));
+
+      alert(divide(number1, number2));
+
+      var subtract = function(number1, number2) {
+      return number1 - number2;
+                  };
+
+      var number1 = parseInt(prompt("Enter a number:"));
+      var number2 = parseInt(prompt("Enter another number:"));
+
+      alert(subtract(number1, number2));
+
+      var multiply = function(number1, number2) {
+      return number1 * number2;
+      };
+      var number1 = parseInt(prompt("Enter a number:"));
+      var number2 = parseInt(prompt("Enter another number:"));
+      alert(multiply(number1, number2));
